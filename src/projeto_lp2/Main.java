9ebdffe079a5bf7075b4fa76acab4de0fffb5766
arthurcavalcant <arthur.ferreira@ccc.ufcg.edu.br>
2019-10-29 08:52:40 -0300

    Adicionei a biblioteca do EasyAccept e as pastas dos testes

--- src/projeto_lp2/Main.java
+++ src/projeto_lp2/Main.java
@@ -1,11 +1,12 @@
 package projeto_lp2;
 
+import easyaccept.EasyAccept;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
-		// Adicionar EasyAccepts
-		
+		args = new String[] {"projeto_lp2.Facade", "TestEasyAccept/use_case_1.txt", "TestEasyAccept/use_case_2.txt",
+				"TestEasyAccept/use_case_3.txt", "TestEasyAccept/use_case_4.txt"};
+		EasyAccept.main(args);
 	}
-	
 }
