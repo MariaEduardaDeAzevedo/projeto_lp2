a0aaaed457996da7134d826769b26dfe2bda4c99
Maria Eduarda de Azevedo <maria-eduarda-azevedo@hotmail.com>
2019-10-29 16:12:42 -0300

    Adicionei testes de unidade para classe Problema

---
+++ src/projeto_lp2/ProblemaTest.java
@@ -0,0 +1,58 @@
+package projeto_lp2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ProblemaTest {
+
+	private Problema problemaComum;
+
+	@BeforeEach
+	void testInit() {
+
+		this.problemaComum = new Problema(
+				"O problema do ainda baixo numero de profissionais mulheres atuando na area de TI", 5);
+
+	}
+
+	@Test
+	void testProblemaComum() {
+
+		Problema problema = new Problema("A problematica de casos de homofobia em servicos de motorista por aplicativo",
+				5);
+		assertEquals("A problematica de casos de homofobia em servicos de motorista por aplicativo - 5",
+				problema.toString());
+
+	}
+
+	@Test
+	void testProblemaDescricaoVazia() {
+
+		assertThrows(IllegalArgumentException.class, () -> {
+			Problema problema = new Problema("", 5);
+		});
+
+	}
+
+	@Test
+	void testProblemaViabilidadeInvalidaMenor() {
+
+		assertThrows(IllegalArgumentException.class, () -> {
+			Problema problema = new Problema("A problematica da supremacia quantica", 0);
+		});
+
+	}
+
+	@Test
+	void testProblemaViabilidadeInvalidaMaior() {
+
+		assertThrows(IllegalArgumentException.class, () -> {
+			Problema problema = new Problema("A problematica da supremacia quantica", 6);
+		});
+
+	}
+
+}
