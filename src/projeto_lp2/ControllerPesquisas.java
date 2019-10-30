aeef4037bf1b79d36e3bcb685908086d68b07e2f
Arthur Silva Cavalcante Ferreira <arthur.ferreira@ccc.ufcg.edu.br>
2019-10-29 23:22:04 -0300

    Documenta as classes da US1

--- src/projeto_lp2/ControllerPesquisas.java
+++ src/projeto_lp2/ControllerPesquisas.java
@@ -1,18 +1,31 @@
 package projeto_lp2;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
-public class ControllerPesquisas extends Validacao{
+/**
+ * Classe Controller responsavel pelos metodos referentes as pesquisas do sistema.
+ */
+public class ControllerPesquisas extends Validacao {
 
+    /**
+     * Armazena um mapa de pesquisas a partir dos seus codigos.
+     */
     private Map<String, Pesquisa> pesquisas;
 
-    public ControllerPesquisas(){
+    /**
+     * Constroi o objeto ControllerPesquisas e inicializa seus atributos.
+     */
+    public ControllerPesquisas() {
         this.pesquisas = new TreeMap<String, Pesquisa>();
     }
 
+    /**
+     * Cadastra uma pesquisa no sistema a partir dos parametros.
+     * @param descricao descricao da pesquisa que se deseja adicionar.
+     * @param campoDeInteresse campo de interesse da pesquisa que se deseja adicionar.
+     * @return String, caso o cadastro da pesquisa seja concluído, representando o codigo da pesquisa.
+     */
     public String cadastraPesquisa(String descricao, String campoDeInteresse) {
         super.validaString(descricao, "Descricao nao pode ser nula ou vazia.");
         super.validaString(campoDeInteresse, "Formato do campo de interesse invalido.");
@@ -20,22 +33,22 @@ public class ControllerPesquisas extends Validacao{
             throw new IllegalArgumentException("Formato do campo de interesse invalido.");
         }
         String[] topicos = campoDeInteresse.split(",");
-        if (topicos.length > 4){
+        if (topicos.length > 4) {
             throw new IllegalArgumentException("Formato do campo de interesse invalido.");
         }
-        for (String topico : topicos){
+        for (String topico : topicos) {
             validaString(topico, "Formato do campo de interesse invalido.");
-            if (topico.length() < 3){
+            if (topico.length() < 3) {
                 throw new IllegalArgumentException("Formato do campo de interesse invalido.");
             }
         }
         String codigo = (campoDeInteresse.substring(0, 3) + "1").toUpperCase();
-        if (pesquisas.containsKey(codigo)){
+        if (pesquisas.containsKey(codigo)) {
             int numero = 1;
-            while (true){
+            while (true) {
                 numero += 1;
                 codigo = (campoDeInteresse.substring(0, 3) + numero).toUpperCase();
-                if (!pesquisas.containsKey(codigo)){
+                if (!pesquisas.containsKey(codigo)) {
                     break;
                 }
             }
@@ -46,34 +59,59 @@ public class ControllerPesquisas extends Validacao{
         return codigo;
     }
 
+    /**
+     * Altera o status de uma pesquisa cadastrada para ativa.
+     * @param codigo codigo da pesquisa que se deseja alterar o status.
+     */
     public void ativaPesquisa(String codigo) {
-        if (!pesquisas.containsKey(codigo)){
+        if (!pesquisas.containsKey(codigo)) {
             throw new IllegalArgumentException("Pesquisa nao encontrada.");
         }
         pesquisas.get(codigo).ativaPesquisa();
     }
 
+    /**
+     * Altera o status de uma pesquisa cadastrada para inativa.
+     * @param codigo codigo da pesquisa que se deseja alterar o status.
+     * @param motivo motivo pelo qual a pesquisa está sendo desativada.
+     */
     public void encerraPesquisa(String codigo, String motivo) {
         super.validaString(codigo, "Codigo nao pode ser nulo ou vazio.");
-        super.hasValor(pesquisas.containsKey(codigo),"Pesquisa nao encontrada.");
+        super.hasValor(pesquisas.containsKey(codigo), "Pesquisa nao encontrada.");
         pesquisas.get(codigo).encerraPesquisa();
     }
 
+    /**
+     * Altera a descricao ou campo de interesse de uma pesquisa cadastrada.
+     * @param codigo codigo da pesquisa que se deseja alterar.
+     * @param conteudoASerAlterado conteudo da pesquisa que se deseja alterar.
+     * @param novoConteudo conteudo que devera substituir o anteriormente cadastrado na pesquisa.
+     */
     public void alteraPesquisa(String codigo, String conteudoASerAlterado, String novoConteudo) {
         super.validaString(codigo, "Codigo nao pode ser nulo ou vazio.");
-        super.hasValor(pesquisas.containsKey(codigo),"Pesquisa nao encontrada.");
+        super.hasValor(pesquisas.containsKey(codigo), "Pesquisa nao encontrada.");
         pesquisas.get(codigo).alteraPesquisa(conteudoASerAlterado, novoConteudo);
     }
 
+    /**
+     * Exibe a representacao de uma pesquisa cadastrada.
+     * @param codigo codigo da pesquisa que se deseja exibir.
+     * @return String contendo a representacao da pesquisa.
+     */
     public String exibePesquisa(String codigo) {
         super.validaString(codigo, "Codigo nao pode ser nulo ou vazio.");
-        super.hasValor(pesquisas.containsKey(codigo),"Pesquisa nao encontrada.");
+        super.hasValor(pesquisas.containsKey(codigo), "Pesquisa nao encontrada.");
         return pesquisas.get(codigo).toString();
     }
 
+    /**
+     * Retorna o status atual da pesquisa cadastrada.
+     * @param codigo codigo da pesquisa que se deseja verificar o status.
+     * @return boolean representando se a pesquisa esta ativa ou nao.
+     */
     public boolean pesquisaEhAtiva(String codigo) {
         super.validaString(codigo, "Codigo nao pode ser nulo ou vazio.");
-        super.hasValor(pesquisas.containsKey(codigo),"Pesquisa nao encontrada.");
+        super.hasValor(pesquisas.containsKey(codigo), "Pesquisa nao encontrada.");
         return pesquisas.get(codigo).isAtivada();
     }
 }
--- src/projeto_lp2/Pesquisa.java
+++ src/projeto_lp2/Pesquisa.java
@@ -1,12 +1,36 @@
 package projeto_lp2;
 
+/**
+ * Representacao de uma Pesquisa, que contem um codigo, descricao, campo de interesse e status.
+ */
 public class Pesquisa extends Validacao {
 
+    /**
+     * Armazena o codigo da pesquisa no sistema.
+     */
     private String codigo;
+
+    /**
+     * Armazena a descricao da pesquisa.
+     */
     private String descricao;
+
+    /**
+     * Armazena o campo de interesse da pesquisa.
+     */
     private String campoDeInteresse;
+
+    /**
+     * Armazena o status da pesquisa.
+     */
     private boolean ativada;
 
+    /**
+     * Constroi o objeto Pesquisa a partir dos parametros.
+     * @param codigo codigo da pesquisa.
+     * @param descricao descricao da pesquisa.
+     * @param campoDeInteresse campo de interese da pesquisa.
+     */
     public Pesquisa(String codigo, String descricao, String campoDeInteresse) {
         super.validaString(codigo, "Codigo nao pode ser nulo ou vazio.");
         super.validaString(descricao, "Descricao nao pode ser nula ou vazia.");
@@ -17,11 +41,18 @@ public class Pesquisa extends Validacao {
         this.ativada = true;
     }
 
+    /**
+     * Retorna a representacao da pesquisa.
+     * @return String contendo a representacao textual de uma pesquisa.
+     */
     @Override
     public String toString() {
         return this.codigo + " - " + this.descricao + " - " + this.campoDeInteresse;
     }
 
+    /**
+     * Altera o status da pesquisa para ativada.
+     */
     public void ativaPesquisa() {
         if (isAtivada()) {
             throw new IllegalArgumentException("Pesquisa ja ativada.");
@@ -29,6 +60,9 @@ public class Pesquisa extends Validacao {
         this.ativada = true;
     }
 
+    /**
+     * Altera o status da pesquisa para desativada.
+     */
     public void encerraPesquisa() {
         if (!isAtivada()) {
             throw new IllegalArgumentException("Pesquisa desativada.");
@@ -36,6 +70,11 @@ public class Pesquisa extends Validacao {
         this.ativada = false;
     }
 
+    /**
+     * Altera alguns atributos da pesquisa.
+     * @param conteudoASerAlterado atributo que se deseja alterar (permitido alterar a descricao ou o campo de interesse).
+     * @param novoConteudo conteudo para qual o atributo deve ser alterado.
+     */
     public void alteraPesquisa(String conteudoASerAlterado, String novoConteudo) {
         if (!isAtivada()) {
             throw new IllegalArgumentException("Pesquisa desativada.");
@@ -51,6 +90,10 @@ public class Pesquisa extends Validacao {
         }
     }
 
+    /**
+     * Retorna o status da pesquisa.
+     * @return boolean representando se a pesquisa esta ativada ou nao.
+     */
     public boolean isAtivada() {
         return ativada;
     }
