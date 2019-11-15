191c75dab385af2cbfb76170fcf80c99171e0aac
Daniel Fonseca <daniel.fonseca@ccc.ufcg.edu.br>
2019-11-15 11:41:44 -0300

    adicionando o resto das funcionalidades da us9 sem estarem adequadas aos testes

--- src/facade/Main.java
+++ src/facade/Main.java
@@ -8,13 +8,8 @@ public class Main {
 		
 		args = new String[] {"facade.Facade", "TestEasyAccept/use_case_1.txt", "TestEasyAccept/use_case_2.txt",
 				"TestEasyAccept/use_case_3.txt", "TestEasyAccept/use_case_4.txt", "TestEasyAccept/use_case_5.txt",
-<<<<<<< HEAD
-				"TestEasyAccept/use_case_6.txt", "TestEasyAccept/use_case_7.txt", "TestEasyAccept/use_case_8.txt", "TestEasyAccept/use_case_9.txt", "TestEasyAccept/use_case_10.txt"
-				,"TestEasyAccept/use_case_11.txt", "TestEasyAccept/use_case_12SALVAR.txt", "TestEasyAccept/use_case_12CARREGAR.txt"};
-=======
 				"TestEasyAccept/use_case_6.txt", "TestEasyAccept/use_case_7.txt", "TestEasyAccept/use_case_8.txt", "TestEasyAccept/use_case_9.txt", "TestEasyAccept/use_case_10.txt",
-				"TestEasyAccept/use_case_11.txt", "TestEasyAccept/use_case_12SALVAR.txt"};
+				"TestEasyAccept/use_case_11.txt", "TestEasyAccept/use_case_12SALVAR.txt", "TestEasyAccept/use_case_12CARREGAR.txt"};
 		EasyAccept.main(args);
->>>>>>> 411e10fbf7b3edbd4cf1fb60303fef35fe626bed
 	}
 }
